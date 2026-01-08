ad599779da458488953eb535f62edebe86fcb873
Alpizar22 <oscar_alpizar@hotmail.com>
2026-01-08 09:04:17 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+function subirDocumento() {
+  const file = document.getElementById("fileInput").files[0];
+
+  if (!file) {
+    alert("Selecciona un archivo");
+    return;
+  }
+
+  document.getElementById("status").innerText =
+    "📤 Documento listo. Próximo paso: conectarlo con AWS...";
+}
 
